c6ef214615ce57cded8ddf29a75df3f22ffff40f
Mateusz Polakowski <mckraqs@gmail.com>
2022-10-25 13:42:17 +0200

    updating network example

--- infra_templates/aws_security_group_rule.tf
+++ infra_templates/aws_security_group_rule.tf
@@ -1,8 +1,15 @@
 resource "aws_security_group_rule" <resource_name> {
   security_group_id = <security_group_id>
+{% if source_security_group_id -%}
+  source_security_group_id = <source_security_group_id>
+{%- endif %}
   type              = <type>
   from_port         = <from_port>
   to_port           = <to_port>
   protocol          = <protocol>
+
   self              = <is_self_ref>
+{%- if prefix_list_ids %}
+  prefix_list_ids   = <prefix_list_ids>
+{%- endif %}
 }
