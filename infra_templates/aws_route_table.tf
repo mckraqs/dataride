4af2bfa429bdba7bdaaea3b33fb62746c662eecf
Mateusz Polakowski <mckraqs@gmail.com>
2022-10-25 12:21:04 +0200

    updating network example

--- infra_templates/aws_route_table.tf
+++ infra_templates/aws_route_table.tf
@@ -1,14 +1,12 @@
 resource "aws_route_table" <resource_name> {
   vpc_id = <vpc_id>
-
-{% if (<gateway_id>, <nat_gateway_id>)|any %}
+{% if (gateway_id, nat_gateway_id)|any %}
   route {
     cidr_block = <basic_route_cidr_block>
-{% if <gateway_id> %}
+{% if gateway_id -%}
     gateway_id = <gateway_id>
-{% elif <nat_gateway_id> %}
+{% elif nat_gateway_id -%}
     nat_gateway_id = <nat_gateway_id>
-{% endif %}
-  }
-{% endif %}
+{% endif -%}}
+{% endif -%}
 }
--- infra_templates/aws_security_group_rule.tf
+++ infra_templates/aws_security_group_rule.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group_rule" <resource_name> {
-  security_group_id = <sg_id>
-  type              = <sgr_type>
+  security_group_id = <security_group_id>
+  type              = <type>
   from_port         = <from_port>
   to_port           = <to_port>
   protocol          = <protocol>
