0ad36c0ced71c6e0fd368cd837d5db5d4561bb04
Mateusz Polakowski <mckraqs@gmail.com>
2022-10-31 13:36:21 +0100

    adding elements tests

---
+++ tests/elements/test_elements.py
@@ -0,0 +1,40 @@
+from dataride.configs.elements.environment import Environment
+from dataride.configs.elements.module import Module
+from dataride.configs.elements.variable import Variable
+from dataride.configs.elements.resource import Resource
+
+
+def test_variable_setup(test_variable_target_name, config_variable, jinja_environment):
+    variable = Variable(test_variable_target_name, config_variable, jinja_environment, verbose=False)
+
+    assert variable.name == config_variable["name"]
+    assert variable.target == test_variable_target_name
+    assert len(variable.template_filled) > 0
+
+
+def test_resource_setup(resource_name, config_resource, jinja_environment):
+    resource = Resource(resource_name, config_resource, jinja_environment, verbose=False)
+
+    assert resource.module == config_resource["_module"]
+    assert len(resource.variables) == 1
+    assert resource.variables[0].name == "database_name"
+    assert resource.variables[0].target == "database_name"
+
+
+def test_module_setup(module_name):
+    module = Module(module_name, verbose=False)
+
+    assert module.name == module_name
+    assert len(module.variables) == 0
+    assert len(module.variables_names) == 0
+    assert len(module.resources) == 0
+
+
+def test_environment_setup(config_environment, jinja_environment):
+    environment = Environment("test_environment", config_environment, jinja_environment, verbose=False)
+
+    assert environment.name == "test_environment"
+    assert len(environment.variables) == 1
+    assert environment.variables[0].name == "var_bucket_aws"
+    assert environment.variables[0].target == "variable_test"
+    assert environment.config["backend"]["type"] == "local"
